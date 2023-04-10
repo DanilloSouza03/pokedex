c8addc2e9c566fdd2269f92b541bb84a81b0ddbf
Danillo <danillodesouza03@gmail.com>
2023-04-10 09:56:52 -0300

    Transformando nossa lista de Pokémon em uma lista de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,35 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((errar) => console.log(errar))
+
+function convertPokemonToLi(pokemon) { 
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" alt="${pokemon.name}">
+                </div> 
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url) // requisição HTTP
+    .then((response) => response.json()) // Convertendo o body para JSON
+    .then((jsonBody) => jsonBody.results) // Pegando o resultado da promise
+    .then((pokemons) => { //  Resultado Array com os 10 pokemons
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon) // concatenando novos li
+        }
+    })
+    .catch((error) => console.log(error))
 
